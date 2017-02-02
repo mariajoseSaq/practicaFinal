cfb1f0e443f4eca7d052605b58a3db5ad7d11713
Maria Jose <Maria Jose@DESKTOP-KE6MSFC>
2017-02-02 20:05:57 +0100

    primera prueba correcto

---
+++ test/Pruebas.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import Controlador.Controlador;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Maria Jose
+ */
+public class Pruebas {
+    
+    public Pruebas() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    
+    
+    @Test
+    public void testEE()
+    {
+     int numFicher=2;
+     int numDatosEle=6;
+     Controlador pr= new Controlador();
+     String resultEsperado="Medio";
+     String rdo=pr.dificultadEntradaExterna(numFicher, numDatosEle);
+        assertEquals(resultEsperado, rdo);
+    }
+    
+    
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
